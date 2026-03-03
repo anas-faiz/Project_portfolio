78312acf45719fa63910b7504384878e00bc6c42
anas-faiz <anasfaiz0811@gmail.com>
2026-03-02 16:37:54 -0800

    search componenrt

--- pagination+search/src/components/Search.tsx
+++ pagination+search/src/components/Search.tsx
@@ -1,13 +1,32 @@
 import { useState } from "react"
+import { products } from "../data/products";
 
 const Search = ()=>{
     
     const [searchInput,setSearchInput] = useState("");
     
+    const filteredcopy = products.filter((p)=>
+        //if(searchInput.trim() === "") return
+        p.title.toLowerCase().includes(searchInput.toLowerCase())
+    )
+
 
     return (
         <div>
-            <input placeholder="search items" className="border p-1 m-1" />
+            <input 
+            value={searchInput}
+            onChange={(e)=>setSearchInput(e.target.value)}
+            placeholder="search items" 
+            className="border p-1 m-1" />
+
+            {
+                filteredcopy.map((i)=>(
+                    <div key={i.id} >
+                        {i.title}
+                    </div>
+                ))
+            }
+        
         </div>
     )
 }
