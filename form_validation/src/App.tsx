7d00c5d1f30121b2057e7d7ac7b90b06dae6f92f
anas-faiz <anasfaiz0811@gmail.com>
2026-03-13 03:08:51 -0700

    types in Feild component

--- form_validation/src/App.tsx
+++ form_validation/src/App.tsx
@@ -1,13 +1,17 @@
 import Field from "./component/Field";
 import { formSchema } from "./schema/formschema";
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div>
-      {
-        formSchema.map(field => <Field key={field.name} {...field} />)
-      }
+      {formSchema.map((field) => (
+        <Field
+          key={field.name}
+          {...field}
+          value={formData[field.name]}
+          onChange={handleChange}
+        />
+      ))}
     </div>
-  )
+  );
 }
